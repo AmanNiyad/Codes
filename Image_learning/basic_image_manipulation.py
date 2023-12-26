368ca93bb746700c5e64be019f9f304f6afbfd0b
AmanNiyad <101011357+AmanNiyad@users.noreply.github.com>
2023-12-26 23:09:52 +0530

    Add files via upload

--- Image_learning/basic_image_manipulation.py
+++ Image_learning/basic_image_manipulation.py
@@ -1,15 +1,15 @@
 from PIL import Image, ImageColor
 
-with Image.open("background_wallpaper_4k_8k___minimalist_landscape_by_jorgehardt_df934b2-pre.png") as image:
+with Image.open("beach-009.jpg") as image:
     #image = image.rotate(60,expand = 1,fillcolor = ImageColor.getcolor('teal','RGB'))
+    print(image.size)
 
-    #image_crop = image.crop(left_x, top_y, right_x, bottom_y)
+    image_crop = image.crop((3, 3, 372, 797))
     #print(type(image))
     #image = image.crop((0,150,image.size[0],400))
     #image_flip_horizontal = image.transpose(Image.Transpose.FLIP_LEFT_RIGHT)
     #image_flip_vertical = image_flip_horizontal.transpose(Image.Transpose.FLIP_TOP_BOTTOM)
 
-    image = image.resize((2560,1440))
+#    image = image.resize((2560,1440))
 
-    image.show()
-    image.save("WALLPAPER1.PNG")
+    image_crop.show()
