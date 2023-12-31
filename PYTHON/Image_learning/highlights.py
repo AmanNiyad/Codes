d2f185be93518a875f3372ec6c163345ee7b37f7
AmanNiyad <101011357+AmanNiyad@users.noreply.github.com>
2023-12-31 23:39:18 +0530

    Optimization needed

--- PYTHON/Image_learning/highlights.py
+++ PYTHON/Image_learning/highlights.py
@@ -1,5 +1,6 @@
 import numpy as np
 from PIL import Image
+from scipy.signal import convolve2d
 
 image = Image.open("beach-009.jpg")
 
@@ -35,15 +36,28 @@ shadows = image.point(lambda i: whatever(i))
 
 #shadows.show()
 """
+
+def mean_values(transpose):
+    kernel = np.ones((25,25))
+    kernel[1,1] =0
+
+    nsum = convolve2d(transpose, kernel, mode ='same', boundary='fill',fillvalue=0)
+    nnei = convolve2d(np.ones(transpose.shape), kernel, mode ='same', boundary='fill',fillvalue=0)
+
+    mean_arr = nsum/nnei
+    return(mean_arr)
+
 #image.show()
 image = image.convert('HSV')
 
 lut = np.asarray(image, dtype = int)
 transpose = lut.T[2]
 threshold = np.max(transpose)
-print(lut.T[2])
 
-lut.T[2] = [[j-30 if j<threshold/2 else j for j in i] for i in transpose]
+mean_arr = mean_values(transpose)
+
+lut.T[2] = [[transpose[i][j]-20 if mean_arr[i][j]<threshold/2 else transpose[i][j] for j in range(len(transpose[i]))] for i in range(len(transpose))]
+
 image_new = Image.fromarray(lut.astype('uint8'), mode = 'HSV')
 image_new = image_new.convert('RGB')
 image_new.save('output.jpg')
