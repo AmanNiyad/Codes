68c9deaf5c815ed2861f8f02fb87b885eb8dfd1d
AmanNiyad <101011357+AmanNiyad@users.noreply.github.com>
2024-01-08 23:48:25 +0530

    Add files via upload

--- PYTHON/Image_learning/highlights.py
+++ PYTHON/Image_learning/highlights.py
@@ -1,62 +1,31 @@
 import numpy as np
+from numpy.fft import fft2, ifft2
 from PIL import Image
-from scipy.signal import convolve2d
 
 image = Image.open("beach-009.jpg")
 
-#result = image.point(lambda i: (i-46)*255/(255-46) if(i > 46) else 0)
-
-#result.show()
-
-"""
-def whatever(i):
-    if(i<85):
-        return (i-2*x)
-#    elif(i>85 and i<=170):
-#        return i
-    else:
-        return(i)
-#result = image.point(lambda i: (i-170)*255/(255-170) if(i > 170) else 255)
-
-#result.show()
-#image.show()
-
-x = 10
-shadows = image.point(lambda i: whatever(i))
-
-#shadows.show()
-
-x = 20
-shadows = image.point(lambda i: whatever(i))
-
-#shadows.show()
-
-x = 30
-shadows = image.point(lambda i: whatever(i))
-
-#shadows.show()
-"""
+def np_fftconvolve(A, B):
+    return np.real(ifft2(fft2(A)*fft2(B, s=A.shape)))
 
 def mean_values(transpose):
     kernel = np.ones((25,25))
     kernel[1,1] =0
 
-    nsum = convolve2d(transpose, kernel, mode ='same', boundary='fill',fillvalue=0)
-    nnei = convolve2d(np.ones(transpose.shape), kernel, mode ='same', boundary='fill',fillvalue=0)
+    nsum = np_fftconvolve(transpose, kernel)
+    nnei = np_fftconvolve(np.ones(transpose.shape), kernel)
 
     mean_arr = nsum/nnei
     return(mean_arr)
 
-#image.show()
 image = image.convert('HSV')
 
 lut = np.asarray(image, dtype = int)
-transpose = lut.T[2]
+
+transpose = lut[..., 2]
 threshold = np.max(transpose)
-
 mean_arr = mean_values(transpose)
-
-lut.T[2] = [[transpose[i][j]-20 if mean_arr[i][j]<threshold/2 else transpose[i][j] for j in range(len(transpose[i]))] for i in range(len(transpose))]
+transpose[mean_arr < threshold/2] -= 20
+lut[..., 2] = transpose
 
 image_new = Image.fromarray(lut.astype('uint8'), mode = 'HSV')
 image_new = image_new.convert('RGB')
