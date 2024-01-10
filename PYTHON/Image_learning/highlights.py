59fedcd785afec218ac618300dd40a02b9ab8904
AmanNiyad <101011357+AmanNiyad@users.noreply.github.com>
2024-01-10 10:12:08 +0530

    Add files via upload

--- PYTHON/Image_learning/highlights.py
+++ PYTHON/Image_learning/highlights.py
@@ -2,6 +2,11 @@ import numpy as np
 from numpy.fft import fft2, ifft2
 from PIL import Image
 
+import subprocess
+
+import cusignal
+import cupy as cp
+
 image = Image.open("beach-009.jpg")
 
 def np_fftconvolve(A, B):
@@ -17,16 +22,40 @@ def mean_values(transpose):
     mean_arr = nsum/nnei
     return(mean_arr)
 
+def mean_values_gpu(transpose):
+    kernel = np.ones((35,35))
+    kernel[1,1] =0
+
+    nsum = cp.asnumpy(cusignal.convolve2d(transpose, kernel, mode='same'))
+    nnei = cp.asnumpy(cusignal.convolve2d(np.ones(transpose.shape), kernel, mode='same'))
+
+    mean_arr = nsum/nnei
+    return(mean_arr)
+
 image = image.convert('HSV')
 
 lut = np.asarray(image, dtype = int)
+print(lut.shape)
 
 transpose = lut[..., 2]
 threshold = np.max(transpose)
-mean_arr = mean_values(transpose)
-transpose[mean_arr < threshold/2] -= 20
-lut[..., 2] = transpose
+try:
+    subprocess.check_output('nvidia-smi')
+    mean_arr = mean_values_gpu(transpose)
+except:
+    mean_arr = mean_values(transpose)
 
+curVal = 30
+
+transpose[mean_arr < threshold/2] -= curVal
+transpose[np.logical_and(threshold/2 < mean_arr, mean_arr < (threshold/2)+1)] -= int(curVal/7)
+transpose[np.logical_and(threshold/2 < mean_arr, mean_arr < (threshold/2)+3)] -= int(curVal/7)
+transpose[np.logical_and(threshold/2 < mean_arr, mean_arr < (threshold/2)+5)] -= int(curVal/8)
+transpose[np.logical_and(threshold/2 < mean_arr, mean_arr < (threshold/2)+7)] -= int(curVal/9)
+transpose[np.logical_and(threshold/2 < mean_arr, mean_arr < (threshold/2)+9)] -= int(curVal/9)
+lut[..., 2] = transpose
+print(lut.shape)
+print(transpose.shape)
 image_new = Image.fromarray(lut.astype('uint8'), mode = 'HSV')
 image_new = image_new.convert('RGB')
 image_new.save('output.jpg')
